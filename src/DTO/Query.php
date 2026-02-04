87ffb91a6f5b7241207e503ea86572b31251fc04
bidb97 <mantisa1824@gmail.com>
2026-02-04 12:30:00 +0300

    Query DTO structure

--- src/DTO/Query.php
+++ src/DTO/Query.php
@@ -3,10 +3,14 @@
 declare(strict_types = 1);
 
 namespace Bidb97\QueryExplain\DTO;
+
 readonly class Query
 {
     public function __construct(
-
-    )
-    {}
+        public string $className,
+        public string $methodName,
+        public ?int $lineNumber,
+        public string $label,
+        public ?string $sqlQuery,
+    ) {}
 }
